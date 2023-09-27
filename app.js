9c2a5e43a73a0a0a3c501ca37bcc1a1f3b701367
KelySaina <kelysaina@gmail.com>
2023-09-27 17:16:35 +0300

    app.app

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send({ msg: "Welcome to nodeJenkins API lastest" })
+    res.send({ msg: "Welcome to nodeJenkins API lastest KelySaina" })
 })
 
 app.post('/inEmployee', (req, res) => {
